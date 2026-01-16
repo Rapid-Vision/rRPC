fb438d483ef20a2c542df754ac67658df315e63a
Mikhail Pankin <mishapankin@gmail.com>
2026-01-16 16:55:51 +0300

    Moved python __init__ generation to internal/gen/python/

--- cmd/client.go
+++ cmd/client.go
@@ -4,12 +4,10 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"strings"
 
 	gogen "github.com/Rapid-Vision/rRPC/internal/gen/go"
 	pygen "github.com/Rapid-Vision/rRPC/internal/gen/python"
 	"github.com/Rapid-Vision/rRPC/internal/parser"
-	"github.com/Rapid-Vision/rRPC/internal/utils"
 	"github.com/spf13/cobra"
 )
 
@@ -97,45 +95,8 @@ func RunClientCmd(cmd *cobra.Command, args []string) error {
 	if err := os.WriteFile(clientPath, []byte(code), 0o644); err != nil {
 		return fmt.Errorf("write output: %w", err)
 	}
-	if err := os.WriteFile(initPath, []byte(buildPythonInit(schema)), 0o644); err != nil {
+	if err := os.WriteFile(initPath, []byte(pygen.GeneratePythonInit(schema)), 0o644); err != nil {
 		return fmt.Errorf("write output: %w", err)
 	}
 	return nil
-}
-
-func buildPythonInit(schema *parser.Schema) string {
-	var b strings.Builder
-	b.WriteString("from .client import RPCClient\n")
-	b.WriteString("from .client import RPCError\n")
-	b.WriteString("from .client import RPCErrorException\n")
-	b.WriteString("from .client import CustomRPCError\n")
-	b.WriteString("from .client import ValidationRPCError\n")
-	b.WriteString("from .client import InputRPCError\n")
-	b.WriteString("from .client import UnauthorizedRPCError\n")
-	b.WriteString("from .client import ForbiddenRPCError\n")
-	b.WriteString("from .client import NotImplementedRPCError\n")
-	for _, model := range schema.Models {
-		className := utils.NewIdentifierName(model.Name).PascalCase() + "Model"
-		b.WriteString("from .client import ")
-		b.WriteString(className)
-		b.WriteString("\n")
-	}
-	b.WriteString("\n__all__ = [\n")
-	b.WriteString("    \"RPCClient\",\n")
-	b.WriteString("    \"RPCError\",\n")
-	b.WriteString("    \"RPCErrorException\",\n")
-	b.WriteString("    \"CustomRPCError\",\n")
-	b.WriteString("    \"ValidationRPCError\",\n")
-	b.WriteString("    \"InputRPCError\",\n")
-	b.WriteString("    \"UnauthorizedRPCError\",\n")
-	b.WriteString("    \"ForbiddenRPCError\",\n")
-	b.WriteString("    \"NotImplementedRPCError\",\n")
-	for _, model := range schema.Models {
-		className := utils.NewIdentifierName(model.Name).PascalCase() + "Model"
-		b.WriteString("    \"")
-		b.WriteString(className)
-		b.WriteString("\",\n")
-	}
-	b.WriteString("]\n")
-	return b.String()
 }
--- internal/gen/python/client.go
+++ internal/gen/python/client.go
@@ -58,6 +58,43 @@ func GenerateClientWithPrefix(schema *parser.Schema, prefix string) (string, err
 	return buf.String(), nil
 }
 
+func GeneratePythonInit(schema *parser.Schema) string {
+	var b strings.Builder
+	b.WriteString("from .client import RPCClient\n")
+	b.WriteString("from .client import RPCError\n")
+	b.WriteString("from .client import RPCErrorException\n")
+	b.WriteString("from .client import CustomRPCError\n")
+	b.WriteString("from .client import ValidationRPCError\n")
+	b.WriteString("from .client import InputRPCError\n")
+	b.WriteString("from .client import UnauthorizedRPCError\n")
+	b.WriteString("from .client import ForbiddenRPCError\n")
+	b.WriteString("from .client import NotImplementedRPCError\n")
+	for _, model := range schema.Models {
+		className := utils.NewIdentifierName(model.Name).PascalCase() + "Model"
+		b.WriteString("from .client import ")
+		b.WriteString(className)
+		b.WriteString("\n")
+	}
+	b.WriteString("\n__all__ = [\n")
+	b.WriteString("    \"RPCClient\",\n")
+	b.WriteString("    \"RPCError\",\n")
+	b.WriteString("    \"RPCErrorException\",\n")
+	b.WriteString("    \"CustomRPCError\",\n")
+	b.WriteString("    \"ValidationRPCError\",\n")
+	b.WriteString("    \"InputRPCError\",\n")
+	b.WriteString("    \"UnauthorizedRPCError\",\n")
+	b.WriteString("    \"ForbiddenRPCError\",\n")
+	b.WriteString("    \"NotImplementedRPCError\",\n")
+	for _, model := range schema.Models {
+		className := utils.NewIdentifierName(model.Name).PascalCase() + "Model"
+		b.WriteString("    \"")
+		b.WriteString(className)
+		b.WriteString("\",\n")
+	}
+	b.WriteString("]\n")
+	return b.String()
+}
+
 func className(name string) string {
 	return utils.NewIdentifierName(name).PascalCase() + "Model"
 }
